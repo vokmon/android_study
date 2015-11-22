7bf41b23c626b5df640ccfff4ec5d71a284dab82
Arnon  Ruangthanawes <vokmon@hotmail.com>
2015-11-22 13:01:31 +1300

    Add log to study Activity life cycle

--- HelloWorldExample/src/com/example/helloworldexample/MainActivity.java
+++ HelloWorldExample/src/com/example/helloworldexample/MainActivity.java
@@ -2,18 +2,58 @@ package com.example.helloworldexample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends Activity {
 
+	String msg = "Android : ";
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Log.d(msg, "The onCreate() event");
     }
 
+    @Override
+    protected void onStart() {
+    	super.onStart();
+    	
+    	Log.d(msg, "The onStart() event");
+    }
+    
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    	
+    	Log.d(msg, "The onResume() event");
+    }
+    
+    @Override
+    protected void onPause() {
+    	// TODO Auto-generated method stub
+    	super.onPause();
+    	Log.d(msg, "The onPause() event");
+    }
+    
+    @Override
+    protected void onStop() {
+    	// TODO Auto-generated method stub
+    	super.onStop();
+    	Log.d(msg, "The onStop() event");
+    }
+    
+    @Override
+    protected void onDestroy() {
+    	// TODO Auto-generated method stub
+    	super.onDestroy();
+    	Log.d(msg, "The onDestroy() event");
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
