fd092f808b6e59b2f9f064ad11afe11ba76e0562
Arnon  Ruangthanawes <vokmon@hotmail.com>
2015-11-28 11:14:25 +1300

    Add custom activity

---
+++ IntentExample/src/com/example/intentexample/CustomActivity.java
@@ -0,0 +1,17 @@
+package com.example.intentexample;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class CustomActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.custom_view);
+		TextView label = (TextView) findViewById(R.id.show_data);
+		Uri url = getIntent().getData();
+		label.setText(url.toString());
+	}
+}
--- IntentExample/src/com/example/intentexample/MainActivity.java
+++ IntentExample/src/com/example/intentexample/MainActivity.java
@@ -11,28 +11,43 @@ import android.widget.Button;
 
 
 public class MainActivity extends Activity {
-	Button b1,b2;
+	Button b1,b2,b3;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
         b1=(Button)findViewById(R.id.button);
+        
         b1.setOnClickListener(new View.OnClickListener() {
-           
+        
            @Override
            public void onClick(View v) {
-              Intent i = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("http://www.example.com"));
+        	   //Need to select the launcher
+              Intent i = new Intent(android.content.Intent.ACTION_VIEW,Uri.parse("http://www.example.com"));
               startActivity(i);
            }
         });
         
         b2=(Button)findViewById(R.id.button2);
         b2.setOnClickListener(new View.OnClickListener() {
+        
            @Override
            public void onClick(View v) {
-              Intent i = new Intent(android.content.Intent.ACTION_VIEW,Uri.parse("tel:9510300000"));
+        	  //show the url to the activity
+              Intent i = new Intent("com.example.intentexample.customactivity.LAUNCH",Uri.parse("http://www.example.com"));
+        	   
+              startActivity(i);
+           }
+        });
+        
+        b3=(Button)findViewById(R.id.button3);
+        b3.setOnClickListener(new View.OnClickListener() {
+        
+           @Override
+           public void onClick(View v) {
+        	   // error because the scheme is http but defined as https
+              Intent i = new Intent("com.example.intentexample.customactivity.LAUNCH",Uri.parse("https://www.example.com"));
               startActivity(i);
            }
         });
