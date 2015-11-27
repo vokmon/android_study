42218155ce57db0d97a4735df33ef844cdcff4d4
Arnon  Ruangthanawes <vokmon@hotmail.com>
2015-11-28 10:40:52 +1300

    Intent example

--- IntentExample/src/com/example/intentexample/MainActivity.java
+++ IntentExample/src/com/example/intentexample/MainActivity.java
@@ -1,17 +1,41 @@
 package com.example.intentexample;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
-
+	Button b1,b2;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    	super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        b1=(Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+           
+           @Override
+           public void onClick(View v) {
+              Intent i = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("http://www.example.com"));
+              startActivity(i);
+           }
+        });
+        
+        b2=(Button)findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+              Intent i = new Intent(android.content.Intent.ACTION_VIEW,Uri.parse("tel:9510300000"));
+              startActivity(i);
+           }
+        });
     }
 
     @Override
